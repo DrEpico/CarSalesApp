a1f9422e0eaff0c095a25e85c18b559ea26db966
Ali <adadashipour@gmail.com>
2024-10-09 00:08:58 +0100

    Add code to update database record 7

--- CarSalesApp/Program.cs
+++ CarSalesApp/Program.cs
@@ -18,3 +18,15 @@ displayCars(cars);
 var newCar = new Car(0, "Corolla", "Toyota", 2023, Fuel.HYBRID, 13995, DateTime.Now, false);
 db.Cars.Add(newCar);
 db.SaveChanges();
+
+//load in car with id 7
+var car = db.Cars
+    .Where(c => c.Id == 7)
+    .SingleOrDefault(); //Get a single record or NULL
+
+Console.WriteLine($"Before change: {car}");
+car.Price = 19500;
+Console.WriteLine($"After change: {car}");
+
+//Save change to memory objects to the database
+db.SaveChanges();
